2a6f8d676d1ab8dbe24a43caf3e580b6304bdafe
Vishal Singh <vishalsingh2376@gmail.com>
2023-12-27 22:05:48 +0530

    add: text styles

---
+++ app/src/main/java/com/vishal2376/snaptick/presentation/common/TextStyles.kt
@@ -0,0 +1,45 @@
+package com.vishal2376.snaptick.presentation.common
+
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.vishal2376.snaptick.R
+
+
+val fontRoboto = FontFamily(Font(R.font.roboto))
+val fontMontserrat = FontFamily(Font(R.font.montserrat))
+
+var h1TextStyle = TextStyle(
+	fontSize = 26.sp,
+	fontFamily = fontMontserrat,
+	fontWeight = FontWeight.Bold
+)
+
+var h2TextStyle = TextStyle(
+	fontSize = 22.sp,
+	fontFamily = fontMontserrat,
+	fontWeight = FontWeight.Bold
+)
+
+val infoTextStyle = TextStyle(
+	fontSize = 20.sp,
+	fontFamily = fontMontserrat,
+	fontWeight = FontWeight.SemiBold
+)
+
+val infoDescTextStyle = TextStyle(
+	fontSize = 14.sp,
+	fontFamily = fontRoboto
+)
+
+var taskDescTextStyle = TextStyle(
+	fontSize = 12.sp,
+	fontFamily = fontRoboto
+)
+
+var taskTextStyle = TextStyle(
+	fontSize = 18.sp,
+	fontFamily = fontRoboto,
+)
