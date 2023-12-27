d2576dee89854054fb050eddb7c5270b974d12eb
Vishal Singh <vishalsingh2376@gmail.com>
2023-12-27 19:51:43 +0530

    add: fonts,colors and icons

--- app/src/main/java/com/vishal2376/snaptick/MainActivity.kt
+++ app/src/main/java/com/vishal2376/snaptick/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
--- app/src/main/java/com/vishal2376/snaptick/ui/theme/Color.kt
+++ app/src/main/java/com/vishal2376/snaptick/ui/theme/Color.kt
@@ -9,3 +9,12 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Blue500 = Color(0xFF161A30)
+val Blue200 = Color(0xFF31304D)
+
+val Red = Color(0xFFEB8080)
+val Green = Color(0xFFC7E9A7)
+val Blue = Color(0xFF40CAE7)
+
+val LightGray = Color(0xFFCCD2D8)
