62b53e49362f1862ba29f0adec2689289103f010
Vishal Singh <vishalsingh2376@gmail.com>
2023-12-28 22:19:17 +0530

    feat: add time formatting func

--- app/src/main/java/com/vishal2376/snaptick/data/model/Task.kt
+++ app/src/main/java/com/vishal2376/snaptick/data/model/Task.kt
@@ -2,6 +2,9 @@ package com.vishal2376.snaptick.data.model
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 @Entity(tableName = "task_table")
 data class Task(
@@ -10,4 +13,16 @@ data class Task(
 	val isCompleted: Boolean,
 	val startTime: Long,
 	val endTime: Long
-)
+) {
+	fun getFormattedTime(): String {
+		val startTimeFormat = formatTime(startTime)
+		val endTimeFormat = formatTime(endTime)
+		return "$startTimeFormat - $endTimeFormat"
+	}
+
+	private fun formatTime(time: Long): String {
+		val sdf = SimpleDateFormat("hh:mm a", Locale.getDefault())
+		val date = Date(time)
+		return sdf.format(date)
+	}
+}
