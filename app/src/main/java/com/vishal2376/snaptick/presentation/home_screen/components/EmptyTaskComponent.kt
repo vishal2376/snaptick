2308c693a06ea51c48f20ef0bc3d1747fffb2633
Vishal Singh <vishalsingh2376@gmail.com>
2023-12-29 16:31:42 +0530

    chore: design empty task screen ui

---
+++ app/src/main/java/com/vishal2376/snaptick/presentation/home_screen/components/EmptyTaskComponent.kt
@@ -0,0 +1,40 @@
+package com.vishal2376.snaptick.presentation.home_screen.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.vishal2376.snaptick.R
+import com.vishal2376.snaptick.presentation.common.h1TextStyle
+
+@Composable
+fun EmptyTaskComponent() {
+	Box(
+		modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center
+	) {
+		Column(
+			horizontalAlignment = Alignment.CenterHorizontally
+		) {
+			Image(
+				painter = painterResource(id = R.drawable.no_tasks), contentDescription = null,
+				modifier = Modifier.size(250.dp)
+			)
+			Text(text = "No Tasks", style = h1TextStyle, color = Color.White)
+		}
+	}
+}
+
+@Preview
+@Composable
+private fun EmptyTaskComponentPreview() {
+	EmptyTaskComponent()
+}
