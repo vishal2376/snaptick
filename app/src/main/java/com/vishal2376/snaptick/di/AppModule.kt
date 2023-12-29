2718d9fbc60f108d835666f48ff4ec3e0854c4fb
Vishal Singh <vishalsingh2376@gmail.com>
2023-12-29 13:17:08 +0530

    chore: add app module

---
+++ app/src/main/java/com/vishal2376/snaptick/di/AppModule.kt
@@ -0,0 +1,39 @@
+package com.vishal2376.snaptick.di
+
+import android.content.Context
+import androidx.room.Room
+import com.vishal2376.snaptick.data.local.TaskDao
+import com.vishal2376.snaptick.data.local.TaskDatabase
+import com.vishal2376.snaptick.repositories.TaskRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+	@Provides
+	@Singleton
+	fun providesLocalDatabase(@ApplicationContext context: Context): TaskDatabase {
+		return Room.databaseBuilder(
+			context, TaskDatabase::class.java, "local_db"
+		).build()
+	}
+
+	@Provides
+	@Singleton
+	fun providesTaskDao(db: TaskDatabase): TaskDao {
+		return db.taskDao()
+	}
+
+	@Provides
+	@Singleton
+	fun providesTaskRepository(dao: TaskDao): TaskRepository {
+		return TaskRepository(dao)
+	}
+
+}
