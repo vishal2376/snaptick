92bbe8be0ac3746fbcc69074f4e4f41f49bffa4d
Vishal Singh <vishalsingh2376@gmail.com>
2024-01-03 18:48:58 +0530

    add: type LocalTime converter

---
+++ app/src/main/java/com/vishal2376/snaptick/domain/converters/LocalTimeConverter.kt
@@ -0,0 +1,18 @@
+package com.vishal2376.snaptick.domain.converters
+
+import androidx.room.TypeConverter
+import java.time.LocalTime
+
+object LocalTimeConverter {
+    @TypeConverter
+    @JvmStatic
+    fun fromString(value: String?): LocalTime? {
+        return value?.let { LocalTime.parse(it) }
+    }
+
+    @TypeConverter
+    @JvmStatic
+    fun toString(localTime: LocalTime?): String? {
+        return localTime?.toString()
+    }
+}
