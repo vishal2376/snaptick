8791b5a6eed8d734d72f6d793d674c926a1d4ff9
Vishal Singh <vishalsingh2376@gmail.com>
2024-01-06 20:52:15 +0530

    update: task table

--- app/src/main/java/com/vishal2376/snaptick/domain/model/Task.kt
+++ app/src/main/java/com/vishal2376/snaptick/domain/model/Task.kt
@@ -4,17 +4,27 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import androidx.room.TypeConverters
 import com.vishal2376.snaptick.domain.converters.LocalTimeConverter
+import com.vishal2376.snaptick.domain.converters.ReminderListConverter
 import java.time.LocalTime
 import java.time.format.DateTimeFormatter
 
 @Entity(tableName = "task_table")
-@TypeConverters(LocalTimeConverter::class)
+@TypeConverters(
+	LocalTimeConverter::class,
+	ReminderListConverter::class
+)
 data class Task(
 	@PrimaryKey(autoGenerate = true) val id: Int = 0,
 	val title: String,
 	val isCompleted: Boolean,
 	val startTime: LocalTime,
-	val endTime: LocalTime
+	val endTime: LocalTime,
+	val reminderList: List<Reminder> = listOf(
+		Reminder(5),
+		Reminder(10),
+		Reminder(15),
+		Reminder(30)
+	)
 ) {
 	fun getFormattedTime(): String {
 		val startTimeFormat = formatTime(startTime)
