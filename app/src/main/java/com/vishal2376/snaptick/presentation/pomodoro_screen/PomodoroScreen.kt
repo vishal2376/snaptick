f5cbcff7f26ebb5d0a43b18efff28200d80c9f84
vishal2376 <vishalsingh2376@gmail.com>
2024-01-14 22:52:42 +0530

    feat: impl basic timer

--- app/src/main/java/com/vishal2376/snaptick/presentation/pomodoro_screen/PomodoroScreen.kt
+++ app/src/main/java/com/vishal2376/snaptick/presentation/pomodoro_screen/PomodoroScreen.kt
@@ -3,16 +3,21 @@ package com.vishal2376.snaptick.presentation.pomodoro_screen
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -21,6 +26,12 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -30,7 +41,9 @@ import com.vishal2376.snaptick.domain.model.Task
 import com.vishal2376.snaptick.presentation.common.taskTextStyle
 import com.vishal2376.snaptick.presentation.common.timerTextStyle
 import com.vishal2376.snaptick.presentation.pomodoro_screen.components.CustomCircularProgressBar
+import com.vishal2376.snaptick.ui.theme.LightGray
 import com.vishal2376.snaptick.ui.theme.SnaptickTheme
+import kotlinx.coroutines.delay
 import java.time.LocalTime
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -39,6 +52,22 @@ fun PomodoroScreen(
 	task: Task,
 	onBack: () -> Unit
 ) {
+
+	var timeLeft by remember {
+		mutableLongStateOf(30)
+	}
+
+	var isPaused by remember {
+		mutableStateOf(false)
+	}
+
+	LaunchedEffect(key1 = timeLeft,key2 = isPaused) {
+		while (timeLeft > 0 && !isPaused) {
+			delay(1000)
+			timeLeft--
+		}
+	}
+
 	Scaffold(topBar = {
 		TopAppBar(
 			modifier = Modifier.padding(8.dp),
@@ -85,13 +114,40 @@ fun PomodoroScreen(
 				contentAlignment = Alignment.Center
 			) {
 				Text(
-					text = "01 : 30",
+					text = "$timeLeft",
 					style = timerTextStyle,
 					color = Color.White
 				)
-				CustomCircularProgressBar()
+				CustomCircularProgressBar(progress = timeLeft.toFloat())
 			}
 
+			Spacer(modifier = Modifier.height(64.dp))
+
+			Row(horizontalArrangement = Arrangement.spacedBy(24.dp)) {
+				FloatingActionButton(
+					onClick = { isPaused = !isPaused },
+					shape = CircleShape,
+					containerColor = MaterialTheme.colorScheme.secondary,
+					contentColor = LightGray
+				) {
+					Icon(
+						imageVector = Icons.Default.PlayArrow,
+						contentDescription = null
+					)
+				}
+
+				FloatingActionButton(
+					onClick = { /*TODO*/ },
+					shape = CircleShape,
+					containerColor = MaterialTheme.colorScheme.secondary,
+					contentColor = LightGray
+				) {
+					Icon(
+						imageVector = Icons.Default.Refresh,
+						contentDescription = null
+					)
+				}
+			}
 			Spacer(modifier = Modifier.height(innerPadding.calculateTopPadding()))
 		}
 	}
