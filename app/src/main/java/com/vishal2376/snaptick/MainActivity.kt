1f0cfe3a261bc9a1e29ca317603eb8d139a9e986
vishal2376 <vishalsingh2376@gmail.com>
2024-01-23 17:56:05 +0530

    feat: impl notification with channel

--- app/src/main/java/com/vishal2376/snaptick/MainActivity.kt
+++ app/src/main/java/com/vishal2376/snaptick/MainActivity.kt
@@ -4,22 +4,45 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import com.vishal2376.snaptick.domain.model.Task
 import com.vishal2376.snaptick.presentation.main.TaskViewModel
 import com.vishal2376.snaptick.presentation.navigation.AppNavigation
 import com.vishal2376.snaptick.ui.theme.SnaptickTheme
+import com.vishal2376.snaptick.util.NotificationHelper
 import dagger.hilt.android.AndroidEntryPoint
+import java.time.LocalTime
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
 	private val taskViewModel by viewModels<TaskViewModel>()
+	private lateinit var notificationHelper: NotificationHelper
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
+
+		// create notification channel
+		notificationHelper = NotificationHelper(applicationContext)
+		notificationHelper.createNotificationChannel()
+
+		//create and show notification
+		val tempTask = Task(
+			id = 0,
+			title = "Demo Notification",
+			isCompleted = false,
+			startTime = LocalTime.now(),
+			endTime = LocalTime.now(),
+			reminder = false,
+			category = "",
+			priority = 0
+		)
+		notificationHelper.createNotification(tempTask)
+
 		setContent {
 			SnaptickTheme(theme = taskViewModel.appState.theme) {
 				AppNavigation(taskViewModel = taskViewModel)
 			}
 		}
+
 	}
 }
---
+++ app/src/main/java/com/vishal2376/snaptick/util/NotificationHelper.kt
@@ -0,0 +1,36 @@
+package com.vishal2376.snaptick.util
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.Context.NOTIFICATION_SERVICE
+import androidx.core.app.NotificationCompat
+import com.vishal2376.snaptick.R
+import com.vishal2376.snaptick.domain.model.Task
+
+const val NOTIFICATION = "Notification"
+const val CHANNEL_ID = "snaptick-notification"
+const val CHANNEL_NAME = "Task Reminder"
+
+class NotificationHelper(private val context: Context) {
+
+	private val notificationManager =
+		context.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+
+	fun createNotification(task: Task) {
+
+		val notificationBuilder = NotificationCompat.Builder(context, CHANNEL_ID)
+			.setContentTitle(task.title)
+			.setSmallIcon(R.drawable.ic_clock)
+			.setStyle(NotificationCompat.BigTextStyle().bigText(task.getFormattedTime()))
+
+		notificationManager.notify(task.id, notificationBuilder.build())
+	}
+
+	fun createNotificationChannel() {
+		val mChannel =
+			NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH)
+		notificationManager.createNotificationChannel(mChannel)
+	}
+}
