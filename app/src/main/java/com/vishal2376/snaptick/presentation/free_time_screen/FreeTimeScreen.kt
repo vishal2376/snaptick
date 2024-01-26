abc2a810e292c3135d57e8f5c99c3a1f3d363ef0
vishal2376 <vishalsingh2376@gmail.com>
2024-01-26 17:39:54 +0530

    add: FreeTimeScreen UI

--- app/src/main/java/com/vishal2376/snaptick/presentation/free_time_screen/FreeTimeScreen.kt
+++ app/src/main/java/com/vishal2376/snaptick/presentation/free_time_screen/FreeTimeScreen.kt
@@ -1,7 +1,16 @@
 package com.vishal2376.snaptick.presentation.free_time_screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -13,13 +22,17 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.vishal2376.snaptick.domain.model.Task
 import com.vishal2376.snaptick.presentation.common.h1TextStyle
 import com.vishal2376.snaptick.presentation.free_time_screen.components.CustomPieChart
+import com.vishal2376.snaptick.presentation.free_time_screen.components.PieChartItemComponent
 import com.vishal2376.snaptick.ui.theme.SnaptickTheme
+import com.vishal2376.snaptick.ui.theme.pieChartColors
 import java.time.LocalTime
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -28,6 +41,7 @@ fun FreeTimeScreen(tasks: List<Task>) {
 
 	val inCompletedTasks = tasks.filter { !it.isCompleted }
 	val sortedTasks = inCompletedTasks.sortedBy { -it.getDuration() }
+	val totalColors = pieChartColors.size
 
 	Scaffold(topBar = {
 		TopAppBar(
@@ -53,8 +67,39 @@ fun FreeTimeScreen(tasks: List<Task>) {
 	}) { innerPadding ->
 
 		Column(modifier = Modifier.padding(innerPadding)) {
-			val data = sortedTasks.map { it.getDuration() }
-			CustomPieChart(data = data)
+
+			val pieChartBgGradient = Brush.verticalGradient(
+				listOf(
+					MaterialTheme.colorScheme.primary,
+					MaterialTheme.colorScheme.secondary,
+				)
+			)
+			Box(
+				modifier = Modifier
+					.fillMaxWidth()
+					.background(
+						pieChartBgGradient,
+						RoundedCornerShape(bottomStart = 32.dp, bottomEnd = 32.dp)
+					)
+					.padding(bottom = 24.dp),
+				contentAlignment = Alignment.Center
+			) {
+				val data = sortedTasks.map { it.getDuration() }
+				CustomPieChart(data = data, pieChartSize = 150.dp)
+			}
+
+			Spacer(modifier = Modifier.height(24.dp))
+
+			LazyColumn(
+				modifier = Modifier
+					.fillMaxSize()
+					.padding(horizontal = 16.dp)
+			) {
+				itemsIndexed(items = sortedTasks) { index, item ->
+					PieChartItemComponent(task = item, itemColor = pieChartColors[index % totalColors])
+					Spacer(modifier = Modifier.height(16.dp))
+				}
+			}
 		}
 	}
 }
