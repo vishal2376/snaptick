69a507e0b31674e5d0e364f0322bbaea399f3a3e
vishal2376 <vishalsingh2376@gmail.com>
2024-01-28 17:46:33 +0530

    chore: add indicator in priority choice

--- app/src/main/java/com/vishal2376/snaptick/presentation/add_edit_screen/components/PriorityComponent.kt
+++ app/src/main/java/com/vishal2376/snaptick/presentation/add_edit_screen/components/PriorityComponent.kt
@@ -1,65 +1,107 @@
 package com.vishal2376.snaptick.presentation.add_edit_screen.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.vishal2376.snaptick.presentation.common.h3TextStyle
-import com.vishal2376.snaptick.ui.theme.Red
 import com.vishal2376.snaptick.ui.theme.SnaptickTheme
+import com.vishal2376.snaptick.ui.theme.priorityColors
+import com.vishal2376.snaptick.util.Priority
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PriorityComponent(
-	title: String,
-	backgroundColor: Color,
-	modifier: Modifier,
-	onClick: () -> Unit
+	defaultSortTask: Priority = Priority.LOW,
+	onSelect: (Priority) -> Unit
 ) {
-	Card(
-		modifier = modifier
-			.fillMaxWidth(),
-		shape = RoundedCornerShape(8.dp),
-		colors = CardDefaults.cardColors(containerColor = backgroundColor),
-		onClick = { onClick() },
 
-		) {
-		Text(
-			text = title,
-			style = h3TextStyle,
-			modifier = Modifier
-				.fillMaxWidth()
-				.wrapContentSize(Alignment.Center)
-				.padding(
-					16.dp,
-					24.dp
-				)
-		)
+	var selectedOption by remember {
+		mutableStateOf(defaultSortTask)
+	}
+
+	Row(
+		modifier = Modifier.fillMaxWidth(),
+		horizontalArrangement = Arrangement.spacedBy(8.dp)
+	) {
+		Priority.entries.forEach {
+			PriorityItemComponent(
+				title = it.displayText,
+				backgroundColor = priorityColors[it.ordinal],
+				isSelected = selectedOption == it,
+				modifier = Modifier.weight(1f)
+			) {
+				selectedOption = it
+				onSelect(it)
+			}
+		}
 	}
 }
 
-@Preview(showSystemUi = true)
+
+@Composable
+fun PriorityItemComponent(
+	title: String,
+	backgroundColor: Color,
+	isSelected: Boolean,
+	modifier: Modifier,
+	onClick: () -> Unit
+) {
+	Column(
+		modifier = modifier.fillMaxWidth(),
+		verticalArrangement = Arrangement.spacedBy(4.dp),
+		horizontalAlignment = Alignment.CenterHorizontally
+	) {
+
+		Box(
+			modifier = Modifier
+				.fillMaxWidth()
+				.background(backgroundColor, RoundedCornerShape(8.dp))
+				.clickable { onClick() },
+			contentAlignment = Alignment.Center
+		) {
+			Text(
+				text = title,
+				style = h3TextStyle,
+				modifier = Modifier.padding(16.dp, 24.dp)
+			)
+		}
+
+		if (isSelected) {
+			Box(
+				modifier = Modifier
+					.width(50.dp)
+					.height(4.dp)
+					.background(backgroundColor, RoundedCornerShape(8.dp))
+			)
+		}
+	}
+}
+
+@Preview()
 @Composable
 fun PriorityComponentPreview() {
 	SnaptickTheme(
 		darkTheme = true,
 		dynamicColor = false
 	) {
-		PriorityComponent(
-			"High",
-			Red,
-			Modifier,
-			{}
-		)
+		PriorityComponent(Priority.LOW, {})
 	}
 }
