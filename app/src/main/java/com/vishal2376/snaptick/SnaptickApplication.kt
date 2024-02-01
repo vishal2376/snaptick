01aeadf064e3aeb7b19e55e37588167ce0879771
vishal2376 <vishalsingh2376@gmail.com>
2024-02-01 16:15:28 +0530

    feat: impl acra for automate crash reports

--- app/src/main/java/com/vishal2376/snaptick/SnaptickApplication.kt
+++ app/src/main/java/com/vishal2376/snaptick/SnaptickApplication.kt
@@ -1,7 +1,41 @@
 package com.vishal2376.snaptick
 
 import android.app.Application
+import android.content.Context
+import com.vishal2376.snaptick.util.Constants
 import dagger.hilt.android.HiltAndroidApp
+import org.acra.ACRA
+import org.acra.BuildConfig
+import org.acra.config.CoreConfigurationBuilder
+import org.acra.config.DialogConfigurationBuilder
+import org.acra.config.MailSenderConfigurationBuilder
+import org.acra.data.StringFormat
 
 @HiltAndroidApp
-class SnaptickApplication : Application()
+class SnaptickApplication : Application() {
+	override fun attachBaseContext(base: Context?) {
+		super.attachBaseContext(base)
+		ACRA.init(
+			this, CoreConfigurationBuilder()
+				.withBuildConfigClass(BuildConfig::class.java)
+				.withReportFormat(StringFormat.JSON)
+				.withPluginConfigurations(
+
+					// Dialog configuration:
+					DialogConfigurationBuilder()
+						.withText(getString(R.string.dialog_text))
+						.withTitle(getString(R.string.dialog_title))
+						.withPositiveButtonText(getString(R.string.dialog_positive))
+						.withNegativeButtonText(getString(R.string.dialog_negative))
+						.build(),
+
+					// Mail sender configuration:
+					MailSenderConfigurationBuilder()
+						.withMailTo(Constants.EMAIL)
+						.withReportFileName("crash_report.txt")
+						.withReportAsFile(true)
+						.build()
+				)
+		)
+	}
+}
--- app/src/main/java/com/vishal2376/snaptick/util/Constants.kt
+++ app/src/main/java/com/vishal2376/snaptick/util/Constants.kt
@@ -1,6 +1,7 @@
 package com.vishal2376.snaptick.util
 
 object Constants {
+	const val EMAIL = "vishalsingh2376@gmail.com"
 	const val LIST_ANIMATION_DELAY = 100
 
 	// DATA STORE KEYS
