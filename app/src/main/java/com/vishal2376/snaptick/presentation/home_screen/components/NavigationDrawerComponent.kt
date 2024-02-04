95d8aa0d524e817e5a10f5d7b4e9464e6be4682c
vishal2376 <vishalsingh2376@gmail.com>
2024-02-05 01:37:49 +0530

    chore: add more nav drawer items

--- app/src/main/java/com/vishal2376/snaptick/presentation/home_screen/components/NavigationDrawerComponent.kt
+++ app/src/main/java/com/vishal2376/snaptick/presentation/home_screen/components/NavigationDrawerComponent.kt
@@ -1,5 +1,7 @@
 package com.vishal2376.snaptick.presentation.home_screen.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -7,9 +9,15 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.BugReport
+import androidx.compose.material.icons.filled.Chat
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
 import androidx.compose.material3.SwitchDefaults
@@ -18,15 +26,22 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.vishal2376.snaptick.presentation.common.h1TextStyle
+import androidx.compose.ui.unit.sp
+import com.vishal2376.snaptick.R
+import com.vishal2376.snaptick.presentation.common.fontRobotoMono
+import com.vishal2376.snaptick.presentation.common.h2TextStyle
+import com.vishal2376.snaptick.presentation.common.h3TextStyle
 import com.vishal2376.snaptick.presentation.common.taskTextStyle
 import com.vishal2376.snaptick.presentation.main.MainEvent
 import com.vishal2376.snaptick.ui.theme.AppTheme
 import com.vishal2376.snaptick.ui.theme.Blue
-import com.vishal2376.snaptick.ui.theme.Red
 
 @Composable
 fun NavigationDrawerComponent(appTheme: AppTheme, onMainEvent: (MainEvent) -> Unit) {
@@ -35,26 +50,43 @@ fun NavigationDrawerComponent(appTheme: AppTheme, onMainEvent: (MainEvent) -> Un
 
 	Column(
 		horizontalAlignment = Alignment.CenterHorizontally,
-		verticalArrangement = Arrangement.Center,
+		verticalArrangement = Arrangement.spacedBy(16.dp),
 		modifier = Modifier
-			.fillMaxWidth(0.8f)
 			.fillMaxHeight()
+			.fillMaxWidth(0.8f)
+			.padding(vertical = 64.dp)
 	) {
 
-		Text(
-			text = "Temporary Design",
-			style = h1TextStyle,
-			color = Color.White
-		)
 		Spacer(modifier = Modifier.height(50.dp))
-
+		Column(
+			horizontalAlignment = Alignment.CenterHorizontally
+		) {
+			Image(
+				painter = painterResource(if (appTheme == AppTheme.Amoled) R.drawable.app_logo_amoled else R.drawable.app_logo),
+				contentDescription = null,
+				modifier = Modifier.size(64.dp),
+			)
+			Spacer(modifier = Modifier.height(8.dp))
+			Text(
+				text = stringResource(R.string.app_name),
+				style = h2TextStyle,
+				color = Color.White
+			)
+			Text(
+				text = "v0.1",
+				fontFamily = fontRobotoMono,
+				fontSize = 15.sp,
+				fontWeight = FontWeight.Bold,
+				color = Color.White
+			)
+		}
 		Row(
 			verticalAlignment = Alignment.CenterVertically,
 			horizontalArrangement = Arrangement.spacedBy(16.dp)
 		) {
 			Text(
 				text = "Amoled Theme",
-				style = taskTextStyle,
+				style = h3TextStyle,
 				color = Color.White
 			)
 			Switch(
@@ -70,18 +102,39 @@ fun NavigationDrawerComponent(appTheme: AppTheme, onMainEvent: (MainEvent) -> Un
 			)
 		}
 
-		Spacer(modifier = Modifier.height(8.dp))
+		Divider(thickness = 2.dp, color = MaterialTheme.colorScheme.secondary)
 
-		Button(
-			onClick = { throw RuntimeException("Crash Testing") },
-			colors = ButtonDefaults.buttonColors(containerColor = Red),
-			shape = RoundedCornerShape(8.dp),
+		Column(
+			modifier = Modifier.padding(start = 32.dp),
+			verticalArrangement = Arrangement.spacedBy(32.dp)
 		) {
-			Text(text = "Crash Testing")
+			NavDrawerItem(icon = Icons.Default.BugReport, label = "Bug Report") {}
+			NavDrawerItem(icon = Icons.Default.Chat, label = "Suggestions") {}
+			NavDrawerItem(icon = Icons.Default.Star, label = "Rate Us") {}
+			NavDrawerItem(icon = Icons.Default.Share, label = "Share App") {}
 		}
 
 	}
 
+}
+
+@Composable
+fun NavDrawerItem(icon: ImageVector, label: String, onClick: () -> Unit) {
+	Row(
+		modifier = Modifier
+			.fillMaxWidth()
+			.clickable { onClick() },
+		horizontalArrangement = Arrangement.spacedBy(10.dp),
+		verticalAlignment = Alignment.CenterVertically
+	) {
+		Icon(
+			modifier = Modifier.size(32.dp),
+			imageVector = icon,
+			contentDescription = null,
+			tint = Color.White
+		)
+		Text(text = label, style = taskTextStyle, color = Color.White)
+	}
 }
 
 @Preview(widthDp = 350)
