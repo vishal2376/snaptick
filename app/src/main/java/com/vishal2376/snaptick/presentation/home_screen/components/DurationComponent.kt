4ea0fd9a23680d4af7c754e60ddb771b530f8b80
vishal2376 <vishalsingh2376@gmail.com>
2024-02-08 19:34:24 +0530

    chore: duration ui component done

--- app/src/main/java/com/vishal2376/snaptick/presentation/home_screen/components/DurationComponent.kt
+++ app/src/main/java/com/vishal2376/snaptick/presentation/home_screen/components/DurationComponent.kt
@@ -1,38 +1,97 @@
 package com.vishal2376.snaptick.presentation.home_screen.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.vishal2376.snaptick.presentation.common.taskTextStyle
 import com.vishal2376.snaptick.ui.theme.SnaptickTheme
 
 @Composable
-fun DurationComponent() {
+fun DurationComponent(
+	durationList: List<Long>,
+	onSelect: (Int) -> Unit
+) {
+	Row(
+		horizontalArrangement = Arrangement.Center,
+		verticalAlignment = Alignment.CenterVertically,
+	) {
+		var selectedOption by remember { mutableIntStateOf(1) }
 
+		val lastIndex = durationList.lastIndex
+		durationList.forEachIndexed { index, it ->
+			var durationText = "$it min"
+
+			val shape = when (index) {
+				0 -> {
+					RoundedCornerShape(topStart = 8.dp, bottomStart = 8.dp)
+				}
+
+				lastIndex -> {
+					durationText = "Custom"
+					RoundedCornerShape(topEnd = 8.dp, bottomEnd = 8.dp)
+				}
+
+				else -> {
+					RectangleShape
+				}
+			}
+
+			DurationItemComponent(
+				modifier = Modifier.weight(1f),
+				text = durationText,
+				isSelected = selectedOption == index,
+				shape = shape
+			) {
+				onSelect(index)
+				selectedOption = index
+			}
+		}
+	}
 }
 
 @Composable
-fun DurationItemComponent() {
-	val roundShape = RoundedCornerShape(8.dp)
-	val startRoundShape = RoundedCornerShape(topStart = 8.dp, bottomStart = 8.dp)
-	val endRoundShape = RoundedCornerShape(bottomEnd = 8.dp, topEnd = 8.dp)
+fun DurationItemComponent(
+	modifier: Modifier = Modifier,
+	text: String,
+	isSelected: Boolean = false,
+	shape: Shape = RectangleShape,
+	onClick: () -> Unit
+) {
+	val bgColor = if (isSelected)
+		MaterialTheme.colorScheme.secondary
+	else
+		MaterialTheme.colorScheme.primary
 
 	Box(
-		modifier = Modifier
-			.border(2.dp, MaterialTheme.colorScheme.secondary, roundShape)
-			.padding(16.dp, 8.dp),
+		modifier = modifier
+			.fillMaxWidth()
+			.background(bgColor, shape)
+			.border(2.dp, MaterialTheme.colorScheme.secondary, shape)
+			.padding(16.dp, 12.dp)
+			.clickable { onClick() },
 		contentAlignment = Alignment.Center
 	) {
-		Text(text = "30 min", color = Color.White, style = taskTextStyle)
+		Text(text = text, color = Color.White, style = taskTextStyle)
 	}
 }
 
@@ -40,7 +99,7 @@ fun DurationItemComponent() {
 @Composable
 fun DurationComponentPreview() {
 	SnaptickTheme {
-		DurationComponent()
+		DurationComponent(durationList = listOf(30, 60, 90, 0), {})
 	}
 }
 
@@ -48,6 +107,6 @@ fun DurationComponentPreview() {
 @Composable
 fun DurationItemComponentPreview() {
 	SnaptickTheme {
-		DurationItemComponent()
+		DurationItemComponent(text = "60 min", onClick = {})
 	}
 }
