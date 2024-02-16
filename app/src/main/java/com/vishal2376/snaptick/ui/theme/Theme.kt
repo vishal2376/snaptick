350a7524f4b68461969ba38d23cdb3c0490e60ac
vishal2376 <vishalsingh2376@gmail.com>
2024-02-16 17:05:38 +0530

    fix #1:status bar color issue

--- app/src/main/java/com/vishal2376/snaptick/ui/theme/Theme.kt
+++ app/src/main/java/com/vishal2376/snaptick/ui/theme/Theme.kt
@@ -1,7 +1,6 @@
 package com.vishal2376.snaptick.ui.theme
 
 import android.app.Activity
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
@@ -45,7 +44,7 @@ enum class AppTheme {
 
 @Composable
 fun SnaptickTheme(
-	darkTheme: Boolean = isSystemInDarkTheme(),
+	darkTheme: Boolean = false,
 	// Dynamic color is available on Android 12+
 	dynamicColor: Boolean = false,
 	theme: AppTheme = AppTheme.Dark,
