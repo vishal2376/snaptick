042adffdbca71992286c9cd0b3075ae43cc64673
vishal2376 <vishalsingh2376@gmail.com>
2024-02-19 11:59:40 +0530

    chore: refactor repeat task logic

--- app/src/main/java/com/vishal2376/snaptick/util/WorkManagerHelper.kt
+++ app/src/main/java/com/vishal2376/snaptick/util/WorkManagerHelper.kt
@@ -7,24 +7,37 @@ import androidx.work.WorkManager
 import com.google.gson.Gson
 import com.vishal2376.snaptick.domain.model.Task
 import com.vishal2376.snaptick.worker.NotificationWorker
-import com.vishal2376.snaptick.worker.RepeatTaskWorker
 import java.time.LocalTime
 import java.util.concurrent.TimeUnit
 
 object WorkManagerHelper {
 
 	fun scheduleNotification(task: Task) {
-		val data = Data.Builder().putString(Constants.TASK_UUID, task.uuid)
-			.putString(Constants.TASK_TITLE, task.title)
-			.putString(Constants.TASK_TIME, task.getFormattedTime())
-			.build()
+		val taskString = Gson().toJson(task)
+		val data = Data.Builder().putString(Constants.TASK, taskString).build()
 
 		val startTimeSec = task.startTime.toSecondOfDay()
 		val currentTimeSec = LocalTime.now().toSecondOfDay()
-		val delaySec = startTimeSec - currentTimeSec
+		var delaySec = startTimeSec - currentTimeSec
 
-		if (delaySec > 0) {
 
+		if (task.isRepeated) {
+			if (delaySec < 0) {
+				delaySec += LocalTime.MAX.toSecondOfDay()
+			}
+
+			val workRequest =
+//				PeriodicWorkRequest.Builder(NotificationWorker::class.java, 1, TimeUnit.DAYS)
+				PeriodicWorkRequest.Builder(NotificationWorker::class.java, 20, TimeUnit.MINUTES)
+					.setInitialDelay(delaySec.toLong(), TimeUnit.SECONDS)
+					.setInputData(data)
+					.addTag(task.uuid)
+					.build()
+
+			// Enqueue the work request with WorkManager
+			WorkManager.getInstance().enqueue(workRequest)
+
+		} else {
 			cancelNotification(task.uuid)
 
 			val workRequest = OneTimeWorkRequest.Builder(NotificationWorker::class.java)
@@ -38,32 +51,8 @@ object WorkManagerHelper {
 		}
 	}
 
-	fun scheduleRepeatTask(task: Task) {
-		val taskString = Gson().toJson(task)
-		val data = Data.Builder().putString(Constants.TASK, taskString).build()
-		val tag = "Repeat-${task.uuid}"
-
-		val midNightSec = LocalTime.MIDNIGHT.toSecondOfDay()
-		val currentTimeSec = LocalTime.now().toSecondOfDay()
-		val delaySec = midNightSec - currentTimeSec
-
-		val workRequest =
-			PeriodicWorkRequest.Builder(RepeatTaskWorker::class.java, 1, TimeUnit.DAYS)
-				.setInitialDelay(delaySec.toLong(), TimeUnit.SECONDS)
-				.setInputData(data)
-				.addTag(tag)
-				.build()
-
-		// Enqueue the work request with WorkManager
-		WorkManager.getInstance().enqueue(workRequest)
-	}
-
 	fun cancelNotification(taskUUID: String) {
 		WorkManager.getInstance().cancelAllWorkByTag(taskUUID)
 	}
 
-	fun cancelRepeatTask(taskUUID: String) {
-		val tag = "Repeat-$taskUUID"
-		WorkManager.getInstance().cancelAllWorkByTag(tag)
-	}
 }
