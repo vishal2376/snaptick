4e1c3d88914fb3b727bf1cc17d6f958e7ec61389
vishal2376 <vishalsingh2376@gmail.com>
2024-02-20 13:02:02 +0530

    upgrade: appVersion to 1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
 		applicationId = "com.vishal2376.snaptick"
 		minSdk = 26
 		targetSdk = 34
-		versionCode = 3
-		versionName = "1.1"
+		versionCode = 4
+		versionName = "1.1.1"
 
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 		vectorDrawables {
