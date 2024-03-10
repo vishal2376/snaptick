87cd78e6459e5414d290c9de8e008c73d0ba19d6
vishal2376 <vishalsingh2376@gmail.com>
2024-03-10 12:09:44 +0530

    upgrade: appVersion to 2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
 		applicationId = "com.vishal2376.snaptick"
 		minSdk = 26
 		targetSdk = 34
-		versionCode = 5
-		versionName = "1.1.2"
+		versionCode = 6
+		versionName = "2.0"
 
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 		vectorDrawables {
