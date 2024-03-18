3b186c56f0165349d35c2c505924c0e474d80ceb
vishal2376 <vishalsingh2376@gmail.com>
2024-03-18 18:12:20 +0530

    chore: add month calender view + animation

--- app/src/main/java/com/vishal2376/snaptick/presentation/calender_screen/CalenderScreen.kt
+++ app/src/main/java/com/vishal2376/snaptick/presentation/calender_screen/CalenderScreen.kt
@@ -1,5 +1,6 @@
 package com.vishal2376.snaptick.presentation.calender_screen
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
@@ -22,27 +23,46 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.kizitonwose.calendar.compose.HorizontalCalendar
 import com.kizitonwose.calendar.compose.WeekCalendar
+import com.kizitonwose.calendar.compose.rememberCalendarState
 import com.kizitonwose.calendar.compose.weekcalendar.rememberWeekCalendarState
+import com.kizitonwose.calendar.core.firstDayOfWeekFromLocale
 import com.vishal2376.snaptick.R
+import com.vishal2376.snaptick.presentation.calender_screen.component.DaysOfWeekTitle
+import com.vishal2376.snaptick.presentation.calender_screen.component.MonthDayComponent
 import com.vishal2376.snaptick.presentation.calender_screen.component.WeekDayComponent
 import com.vishal2376.snaptick.presentation.common.h1TextStyle
 import com.vishal2376.snaptick.ui.theme.SnaptickTheme
-import java.time.DayOfWeek
 import java.time.LocalDate
+import java.time.YearMonth
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CalenderScreen(onBack: () -> Unit) {
+	var isWeekCalender by remember { mutableStateOf(false) }
+	val firstDayOfWeek = remember { firstDayOfWeekFromLocale() }
+
+	// monthly calender
+	val currentMonth = remember { YearMonth.now() }
+	val startMonth = remember { currentMonth.minusMonths(300) }
+	val endMonth = remember { currentMonth.plusMonths(300) }
+	val monthState = rememberCalendarState(
+		startMonth = startMonth,
+		endMonth = endMonth,
+		firstVisibleMonth = currentMonth,
+		firstDayOfWeek = firstDayOfWeek
+	)
+
+	// weekly calender
 	val currentDate = remember { LocalDate.now() }
 	val startDate = remember { currentDate.minusDays(200) }
 	val endDate = remember { currentDate.plusDays(200) }
 	var selection by remember { mutableStateOf<LocalDate>(currentDate) }
-
-	val state = rememberWeekCalendarState(
+	val weekState = rememberWeekCalendarState(
 		startDate = startDate,
 		endDate = endDate,
-		firstDayOfWeek = DayOfWeek.MONDAY
+		firstDayOfWeek = firstDayOfWeek
 	)
 
 	Scaffold(topBar = {
@@ -69,15 +89,33 @@ fun CalenderScreen(onBack: () -> Unit) {
 
 		Column(modifier = Modifier.padding(innerPadding)) {
 
-			WeekCalendar(
-				modifier = Modifier.padding(horizontal = 10.dp),
-				state = state,
-				dayContent = { day ->
-					WeekDayComponent(day.date, selected = selection == day.date) {
-						selection = it
+			AnimatedVisibility(visible = isWeekCalender) {
+				WeekCalendar(
+					modifier = Modifier.padding(horizontal = 10.dp),
+					state = weekState,
+					dayContent = { day ->
+						WeekDayComponent(day.date, selected = selection == day.date) {
+							selection = it
+						}
+					},
+				)
+			}
+
+			AnimatedVisibility(visible = !isWeekCalender) {
+				HorizontalCalendar(
+					modifier = Modifier.padding(horizontal = 10.dp),
+					state = monthState,
+					dayContent = { day ->
+						MonthDayComponent(day, selected = selection == day.date) {
+							selection = it
+						}
+					},
+					monthHeader = { month ->
+						val daysOfWeek = month.weekDays.first().map { it.date.dayOfWeek }
+						DaysOfWeekTitle(daysOfWeek = daysOfWeek)
 					}
-				},
-			)
+				)
+			}
 
 			Divider(
 				modifier = Modifier.padding(vertical = 24.dp),
---
+++ app/src/main/java/com/vishal2376/snaptick/presentation/calender_screen/component/MonthDayComponent.kt
@@ -0,0 +1,79 @@
+package com.vishal2376.snaptick.presentation.calender_screen.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.kizitonwose.calendar.core.CalendarDay
+import com.kizitonwose.calendar.core.DayPosition
+import com.vishal2376.snaptick.presentation.common.taskTextStyle
+import com.vishal2376.snaptick.ui.theme.Black500
+import com.vishal2376.snaptick.ui.theme.Blue
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.format.TextStyle
+import java.util.Locale
+
+@Composable
+fun MonthDayComponent(
+	day: CalendarDay,
+	selected: Boolean,
+	onClick: (LocalDate) -> Unit = {}
+) {
+	Box(
+		modifier = Modifier
+			.aspectRatio(1f)
+			.padding(6.dp)
+			.clip(RoundedCornerShape(8.dp))
+			.background(if (selected) Blue else Color.Transparent)
+			.border(
+				if (day.date == LocalDate.now()) 2.dp else (-1).dp,
+				Blue,
+				RoundedCornerShape(8.dp)
+			)
+			.clickable { onClick(day.date) },
+		contentAlignment = Alignment.Center
+	) {
+		Text(
+			text = day.date.dayOfMonth.toString(),
+			style = taskTextStyle,
+			color = if (selected) Black500 else MaterialTheme.colorScheme.onPrimary
+		)
+	}
+}
+
+@Composable
+fun DaysOfWeekTitle(daysOfWeek: List<DayOfWeek>) {
+	Row(modifier = Modifier.fillMaxWidth()) {
+		for (dayOfWeek in daysOfWeek) {
+			Text(
+				modifier = Modifier.weight(1f),
+				textAlign = TextAlign.Center,
+				style = taskTextStyle,
+				color = MaterialTheme.colorScheme.onSecondary,
+				text = dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.getDefault()),
+			)
+		}
+	}
+}
+
+@Preview
+@Composable
+fun MonthDayComponentPreview() {
+	MonthDayComponent(CalendarDay(LocalDate.now(), DayPosition.MonthDate), true)
+}
