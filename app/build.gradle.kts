c0ca18c48bab63ffff56c160406d156ccca2647d
Vishal Singh <vishalsingh2376@gmail.com>
2025-04-14 23:34:07 +0530

    upgrade: appVersion to 3.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
 		applicationId = "com.vishal2376.snaptick"
 		minSdk = 26
 		targetSdk = 34
-		versionCode = 10
-		versionName = "3.2"
+		versionCode = 11
+		versionName = "3.3"
 
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 		vectorDrawables {
